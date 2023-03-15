64b567aaca7d8aa17d0486750255cb28fe810a49
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-03-14 21:34:22 -0300

    agrego estilos

--- src/components/AgendaCard.js
+++ src/components/AgendaCard.js
@@ -3,14 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export function AgendaCard({item}){
     return(
-        <div className="flex">
-            <form>
-            <input type="checkbox" className='p-1'/>
-            <span value={item} > {item.title} </span>
-            <input 
-             type="submit" />
-          
-            <span className='text-danger'>Eliminar</span>
+        <div className="flex bg-light border shadow p-2 mb-3 bg-body rounded  ">
+            <form >
+            <input  type="checkbox" className='p-1'/>
+            <span className="flex p-1 ml-2" value={item} > {item.title}  </span>
+            <span className="text-danger ">Eliminar</span>
             </form>
         </div>
 
--- src/components/AgendaGrid.js
+++ src/components/AgendaGrid.js
@@ -39,9 +39,9 @@ export default function AgendaGrid (){
 
             <div>
                 {agendados.map(item =>(
-                    
+                        <div className='p-2'> 
                         <AgendaCard  key={item.id} item={item} />
-                     
+                        </div>
                     
                 ))
                 
