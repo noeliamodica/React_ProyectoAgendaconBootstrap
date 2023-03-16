4055a6681829f3738bb15d4a795067b3e72103db
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-03-16 19:14:06 -0300

    creo base de emojis

---
+++ src/data.js
@@ -0,0 +1,33 @@
+export const data = [
+    {
+        symbol: "⏰",
+        name: "alarm",
+        keywords: "despertador",
+
+    },
+    {
+        symbol: "🕛",
+        name: "twelve o'clock",
+        keywords: "doce en punto",
+
+    },
+    {
+        symbol: "🕧",
+        name: "twelve thirty",
+        keywords: "doce y treinta",
+
+    },
+    {
+        symbol: "🕐",
+        name: "twelve o'clock",
+        keywords: "una en punto",
+
+    },
+    {
+        symbol: "🕜",
+        name: "one thirty",
+        keywords: "una y media",
+
+    }
+
+]
