4e8373485097c50f5714e26cc9465a2457f5e3db
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-03-16 21:43:09 -0300

    funcionalidad search

--- src/components/AgendaEmojis.js
+++ src/components/AgendaEmojis.js
@@ -1,10 +1,55 @@
-import { forwardRef } from "react"
-
+import { forwardRef, useState } from "react"
+import { data as emojiList } from "../data";
+import { EmojiSearch } from "./EmojiSearch";
 
 export  function AgendaEmojis (props, inputRef){
+
+    const [isOpen, setIsOpen]= useState(false);
+    const [emojis, setEmojis]= useState(emojiList)
+
+    function handleClickOpen(){
+        setIsOpen(!isOpen)
+    }
+
+    function handleSearch(e){
+        const q = e.target.value.toLowerCase();
+        //si esxiste un valor
+        
+        if (!!q) {
+            const search = emojiList.filter((emoji) => {
+                return (emoji.name.toLowerCase().includes(q) || emoji.keywords.toLowerCase().includes(q)
+                );
+            });
+            setEmojis(search); 
+        } else{
+            setEmojis(emojiList);
+        }
+
+    }
+
+    //function EmojiContainer (){
+      //  return (
+      //      <div>
+      //          <EmojiSearch onSearch={handleSearch} />
+       //         <div>
+        //        {emojiList.map(emoji =>
+         //           <div> {emoji.symbol} </div> ) }
+          //      </div>
+           // </div>
+       // )
+   // }
+
     return(
         <div>
-            <button> 😊 </button>
+            <button onClick={handleClickOpen}> 😊 </button>
+            { isOpen ? <div>
+                <EmojiSearch onSearch={handleSearch} />
+                <div>
+                {emojiList.map(emoji =>
+                    <div> {emoji.symbol} </div> ) }
+                </div>
+            </div>
+                : ""}
         </div>
     )
 }
