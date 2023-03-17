af50fc2d72d9b25782b711c1a8be1785004b833c
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-03-17 10:02:34 -0300

    completo base

--- src/data.js
+++ src/data.js
@@ -1,8 +1,8 @@
 export const data = [
     {
         symbol: "⏰",
-        name: "alarm",
-        keywords: "despertador",
+        name: "despertador",
+        keywords: "alarm",
 
     },
     {
@@ -19,7 +19,7 @@ export const data = [
     },
     {
         symbol: "🕐",
-        name: "twelve o'clock",
+        name: "one o'clock",
         keywords: "una en punto",
 
     },
@@ -28,6 +28,127 @@ export const data = [
         name: "one thirty",
         keywords: "una y media",
 
-    }
+    },
+    {
+        symbol: "🕑",
+        name: "two o'clock",
+        keywords: "dos en punto",
+
+    },
+    {
+        symbol: "🕝",
+        name: "two thirty",
+        keywords: "dos y media",
+
+    },
+    {
+        symbol: "🕒",
+        name: "three o'clock",
+        keywords: "tres en punto",
+
+    },
+    {
+        symbol: "🕞",
+        name: "three thirty",
+        keywords: "tres y media",
+
+    },
+    {
+        symbol: "🕓",
+        name: "four o'clock",
+        keywords: "cuatro en punto",
+
+    },
+    {
+        symbol: "🕟",
+        name: "four thirty",
+        keywords: "cuatro y media",
+
+    },
+    {
+        symbol: "🕔",
+        name: "five o'clock",
+        keywords: "cinco en punto",
+
+    },
+    {
+        symbol: "🕠",
+        name: "five thirty",
+        keywords: "cinco y media",
+
+    },
+    {
+        symbol: "🕕",
+        name: "six o'clock",
+        keywords: "seis en punto",
+
+    },
+    {
+        symbol: "🕡",
+        name: "six thirty",
+        keywords: "seis y media",
+
+    },
+    {
+        symbol: "🕖",
+        name: "seven o'clock",
+        keywords: "siete en punto",
+
+    },
+    {
+        symbol: "🕢",
+        name: "seven thirty",
+        keywords: "siete y media",
+
+    },
+    {
+        symbol: "🕗",
+        name: "eigth o'clock",
+        keywords: "ocho en punto",
+
+    },
+    {
+        symbol: "🕣",
+        name: "eigth thirty",
+        keywords: "ocho y media",
+
+    },
+    {
+        symbol: "🕘",
+        name: "nine o'clock",
+        keywords: "nueve en punto",
+
+    },
+    {
+        symbol: "🕤",
+        name: "nine thirty",
+        keywords: "nueve y media",
+
+    },
+    {
+        symbol: "🕙",
+        name: "ten o'clock",
+        keywords: "diez en punto",
+
+    },
+    {
+        symbol: "🕥",
+        name: "ten thirty",
+        keywords: "diez y media",
+
+    },
+    {
+        symbol: "🕚",
+        name: "eleven o'clock",
+        keywords: "once en punto",
+
+    },
+    {
+        symbol: "🕦",
+        name: "eleven o'thirty",
+        keywords: "once y media",
+
+    },
+
 
 ]
